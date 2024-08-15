88da228dc4c54b83a9ee481c7905b65984a373d5
phR0ze <prc451@gmail.com>
2024-08-14 23:12:16 -0600

    Learning nom

--- exif/src/jpeg.rs
+++ exif/src/jpeg.rs
@@ -1,8 +1,17 @@
+use nom::{error::VerboseError, IResult};
 use std::io;
+
+// Custom nom error type
+type Res<T, U> = IResult<T, U, VerboseError<T>>;
 
 // JPEG files can contain a variety of different metadata formats, including JFIF, Exif, IPTC, and XMP.
 pub fn exif<T: io::Read>(reader: T) {
     // Check if the file is a JPEG
+    file_type("hellow").unwrap();
 
     println!("Hello, world!");
+}
+
+pub fn file_type(input: &str) -> IResult<&str, &str> {
+    Ok((input, ""))
 }
--- exif/tests/integration_tests.rs
+++ exif/tests/integration_tests.rs
@@ -1,5 +1,17 @@
 use libexif::prelude::*;
+use nom::{bytes::complete::tag, IResult};
 use std::{fs::File, path::Path};
+
+fn parser(input: &str) -> IResult<&str, &str> {
+    tag("abc")(input)
+}
+
+#[test]
+fn test_file_type() {
+    let (remaining_input, output) = parser("abcHello").unwrap();
+    assert_eq!(remaining_input, "Hello");
+    assert_eq!(output, "abc");
+}
 
 #[test]
 fn test_jpeg() {
